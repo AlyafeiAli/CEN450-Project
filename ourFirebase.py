d7877e8a63148ab655fc59db1f79bdb7a6e64843
AlyafeiAli <52626961+AlyafeiAli@users.noreply.github.com>
2022-07-15 22:51:24 +0400

    Update ourFirebase.py

--- ourFirebase.py
+++ ourFirebase.py
@@ -2,22 +2,26 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 from imgur import *
+import os
 
 # Fetch the service account key JSON file contents
-cred = credentials.Certificate("firebase/firebase.json")
+data = os.path.abspath(os.path.dirname(__file__)) + "/firebase.json"
+cred = credentials.Certificate(data)
 # Initialize the app with a service account, granting admin privileges
 firebase_admin.initialize_app(cred, {
-    'databaseURL': "https://xxx.firebaseio.com"
+    'databaseURL': "https://xxx.firebaseio.com/"
 })
 
 
 # print(ref.get())
+childpath = os.path.abspath(os.path.dirname(__file__)) + "/childname.txt"
+child = open(childpath, "r").readline()
 
-def send(child, sentence, imgname, keyboard=False):
+def send(sentence, imgname, keyboard=False):
     # txt = db.reference('parent/child/' + child + "/reports/")
     # txt.push({"text": sentence, "image": imgname})
     db.reference('parent/child/' + child).update({"name": child, "hasKeyboard": keyboard})
-    db.reference('parent/child/' + child + '/reports/').push({"text": sentence, "image":imagelink(imgname)})
+    db.reference('parent/child/' + child + '/reports/').push({"text": sentence, "imageURL":imagelink(imgname)})
     print("Sent to DB")
     # print(ref.get())
 
